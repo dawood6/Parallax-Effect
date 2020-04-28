dc054041ebf6cb67760bd564b9e943c64293c7cd
dawood6 <dawoodsadiq171@gmail.com>
2020-04-28 23:54:06 +0500

    Feat: Added New Features

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var scrollPos = 0;
-window.addEventListener("scroll", function () {
-  if (document.body.getBoundingClientRect().top > scrollPos) console.log("up");
-  else console.log("dowm");
-  scrollPos = document.body.getBoundingClientRect().top;
-});
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  if (document.body.scrollTop < 120 || document.documentElement.scrollTop === 120 ) {
+    document.getElementById("myh1").className = "slideUp";
+  } else  {
+    document.getElementById("myh1").className = "";
+  } if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350 ) {
+    document.getElementById("myh1").className = "animation";
+  }
+}
